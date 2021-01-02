39e8bd5570301fdbc811a5fb0767f78b7cb2333a
Devulak <Devulak@users.noreply.github.com>
2021-01-02 21:46:17 +0100

    Update Froggy.py

--- Froggy.py
+++ Froggy.py
@@ -5,13 +5,16 @@ import discord
 #from pyyoutube import Api
 import datetime
 import os
+import random
+import googleapiclient.discovery
+from urllib.parse import parse_qs, urlparse
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user} my dudes'.format(client))
     client.loop.create_task(status_task())
 
 
@@ -22,7 +25,7 @@ async def on_message(message):
 
     if message.content.lower().find('wednesday') != -1:
         if datetime.datetime.today().weekday() == 2:
-            await message.channel.send('https://www.youtube.com/watch?v=OzQ-KvxLVT0')
+            await message.channel.send(f'https://www.youtube.com/watch?v={random.choice(PLAYLISTITEMS)["snippet"]["resourceId"]["videoId"]}')
         else:
             await message.channel.send('Not yet my dude')
 
@@ -33,7 +36,7 @@ async def on_message(message):
         await message.channel.send('Thanks, you too, human')
 
     if message.content.lower().find('bad bot') != -1:
-        await message.channel.send('Fuck off, you spooky midget')
+        await message.channel.send('You\'ve been added to the list.')
 
 
 async def status_task():
@@ -43,14 +46,36 @@ async def status_task():
             if is_wednesday is False:
                 is_wednesday = True
                 channel = client.get_channel(758316238744453160)
-                await channel.send("https://www.youtube.com/watch?v=OzQ-KvxLVT0")
+                await channel.send(f'https://www.youtube.com/watch?v={random.choice(PLAYLISTITEMS)["snippet"]["resourceId"]["videoId"]}')
         else:
             is_wednesday = False
         await asyncio.sleep(10)
         
 
-TOKEN = input("FROGGY_DISCORD_BOT_TOKEN not set, please input discord token: ")
-client.run(TOKEN)
+async def populate_playlist_items():
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey = APIKEY)
 
-# playlist = Playlist('https://www.youtube.com/playlist?list=PLphs3AfjTweSmfJwgg-hQfJgA3HHmrY2i')
-# print('Number of videos in playlist: %s' % len(playlist.video_urls))as
+    request = youtube.playlistItems().list(
+        part = "snippet",
+        playlistId = PLAYLIST,
+        maxResults = 50
+    )
+    response = request.execute()
+
+    PLAYLISTITEMS = []
+    while request is not None:
+        response = request.execute()
+        PLAYLISTITEMS += response["items"]
+        request = youtube.playlistItems().list_next(request, response)
+
+    print(f"total: {len(PLAYLISTITEMS)}")
+
+
+TOKEN = input("DC_BOT_TOKEN not set, please provide: ")
+PLAYLIST = input("PLAYLIST_ID not set, please provide: ")
+APIKEY = input("YOUTUBE_v3_API_KEY not set, please provide: ")
+PLAYLISTITEMS = []
+populate_playlist_items()
+
+
+client.run(TOKEN)
