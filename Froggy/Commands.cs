6b5ffa3dfdb79220f679865c1a8b3cea2d7b8774
ZeroXitreo <3112566+ZeroXitreo@users.noreply.github.com>
2024-03-28 17:11:12 +0100

    add inspiration command

---
+++ Froggy/Commands.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Interactions;
+
+namespace Froggy;
+public class Commands : InteractionModuleBase
+{
+	private static readonly HttpClient httpClient = new()
+	{
+		BaseAddress = new Uri("https://inspirobot.me/")
+	};
+
+	[SlashCommand("inspire", "Get an inspirational quote")]
+	public async Task Inspire()
+	{
+		using HttpResponseMessage response = await httpClient.GetAsync("api?generate=true");
+		string responseBody = await response.Content.ReadAsStringAsync();
+		using HttpResponseMessage response2 = await httpClient.GetAsync(responseBody);
+		await RespondWithFileAsync(response2.Content.ReadAsStream(), "inspiration.jpg");
+	}
+}
--- Froggy/Program.cs
+++ Froggy/Program.cs
@@ -1,50 +1,61 @@
 ﻿using Discord;
+using Discord.Interactions;
 using Discord.WebSocket;
 using Froggy;
 using Google.Apis.Services;
 using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
 using Newtonsoft.Json;
+using System.Reflection;
 
 public class Program
 {
 	public static Task Main() => new Program().MainAsync();
 
-	private AppSettings? appSettings;
+	private readonly AppSettings appSettings = JsonConvert.DeserializeObject<AppSettings>(File.ReadAllText("appsettings.json"))!;
 
-	private static readonly DiscordSocketConfig config = new()
+	private readonly DiscordSocketClient client = new(new()
 	{
 		GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
-	};
-
-	private readonly DiscordSocketClient client = new(config);
+	});
 
 	private IList<PlaylistItem> playlist = [];
 
 	private static readonly Random random = new();
 
-	private static readonly HttpClient httpClient = new() { BaseAddress = new Uri("https://inspirobot.me/") };
-
 	public async Task MainAsync()
 	{
-		appSettings = JsonConvert.DeserializeObject<AppSettings>(File.ReadAllText("appsettings.json"));
 		await PopulatePlaylistAsync();
 
 		client.MessageReceived += MessageReceived;
-		await client.LoginAsync(TokenType.Bot, appSettings?.DiscordToken);
+		client.Ready += Ready;
+		await client.LoginAsync(TokenType.Bot, appSettings.DiscordToken);
 		await client.StartAsync();
 		await Task.Delay(-1);
+	}
+
+	private async Task Ready()
+	{
+		InteractionService interactionService = new(client);
+		await interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+		await interactionService.RegisterCommandsGloballyAsync();
+
+		client.InteractionCreated += async interaction =>
+		{
+			var ctx = new SocketInteractionContext(client, interaction);
+			await interactionService.ExecuteCommandAsync(ctx, null);
+		};
 	}
 
 	private async Task PopulatePlaylistAsync()
 	{
 		YouTubeService service = new(new BaseClientService.Initializer()
 		{
-			ApiKey = appSettings?.YtApiKey,
+			ApiKey = appSettings.YtApiKey,
 		});
 
 		PlaylistItemsResource.ListRequest playlistRequest = service.PlaylistItems.List("snippet");
-		playlistRequest.PlaylistId = appSettings?.YtPlaylist;
+		playlistRequest.PlaylistId = appSettings.YtPlaylist;
 		playlistRequest.MaxResults = 50;
 		PlaylistItemListResponse playlistResponse = await playlistRequest.ExecuteAsync();
 		playlist = playlistResponse.Items;
@@ -53,12 +64,9 @@ public class Program
 	private async Task MessageReceived(SocketMessage message)
 	{
 		Console.WriteLine(message.Content);
-		Console.WriteLine(message.ToString());
 		if (message.Author == client.CurrentUser) return;
 
-		await Message(message);
-
-		if (message.MentionedUsers.Any(user => user.Id == client.CurrentUser.Id))
+		if (message.Content.Contains($"<@{client.CurrentUser.Id}>"))
 		{
 			if (DateTime.UtcNow.DayOfWeek == DayOfWeek.Wednesday)
 			{
@@ -68,18 +76,6 @@ public class Program
 			{
 				await message.Channel.SendMessageAsync($"Not wednesday, sorry <@{message.Author.Id}>");
 			}
-		}
-	}
-
-	private async Task Message(SocketMessage message)
-	{
-		if (message.Content == "!inspireme")
-		{
-			await message.Channel.TriggerTypingAsync();
-			using HttpResponseMessage response = await httpClient.GetAsync("api?generate=true");
-			string responseBody = await response.Content.ReadAsStringAsync();
-			using HttpResponseMessage response2 = await httpClient.GetAsync(responseBody);
-			await message.Channel.SendFileAsync(response2.Content.ReadAsStream(), "inspiration.jpg");
 		}
 	}
 }
